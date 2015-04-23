7c5838afdb84497bdd2eee3e236bc75804147174
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-23 14:46:35 -0700

    regular code commit

--- client/index.js
+++ client/index.js
@@ -8,7 +8,10 @@ angular.module('checklist', ['firebase', 'ui.router'])
   .state('home', {url: '/', templateUrl: '/views/home/home.html'})
   .state('faq', {url: '/faq', templateUrl: '/views/home/faq.html'})
   .state('contact', {url: '/contact', templateUrl: '/views/home/contact.html'})
-  .state('about', {url: '/about', templateUrl: '/views/home/about.html'});
+  .state('about', {url: '/about', templateUrl: '/views/home/about.html'})
+  .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'});
+}])
+.run(['$rootScope', '$window', '$firebaseAuth', 'firebaseUrl', function($rootScope, $window, $firebaseAuth, firebaseUrl){
+  $rootScope.fbRoot = new $window.Firebase(firebaseUrl);
+  $rootScope.afAuth = $firebaseAuth($rootScope.fbRoot);
 }]);
-
-//https://angulardo.firebaseio.com/
