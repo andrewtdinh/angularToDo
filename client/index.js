0556ebb06d92f48c6c3dbc318b0a965ac8b9a7dd
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-23 12:05:38 -0700

    Nav bar added

--- client/index.js
+++ client/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-angular.module('poseidon', ['firebase']);
+angular.module('checklist', ['firebase', 'ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {url: '/', templateUrl: '/views/home/home.html'})
+  .state('faq', {url: '/faq', templateUrl: '/views/home/faq.html'})
+  .state('contact', {url: '/contact', templateUrl: '/views/home/contact.html'})
+  .state('about', {url: '/about', templateUrl: '/views/home/about.html'});
+}]);
+
+//https://angulardo.firebaseio.com/
